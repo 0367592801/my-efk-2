229984bf49ad0f3372b280ac24cbc7e588021b6d
minhpt <phantuanminhnh117@gmail.com>
2021-05-16 21:35:11 +0700

    new server

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a271ab110c123ef42611989d3c98c424'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e1dc2b99-5df9-40a2-b788-2f3f3d4006fc'
+};
